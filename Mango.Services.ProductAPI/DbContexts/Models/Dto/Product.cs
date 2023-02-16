e47ab293b82ff1150beb1db2614cf670485d115f
David Oh <DavidOh@protonmail.com>
2023-02-15 19:30:40 -0800

    DbContext update database

--- Mango.Services.ProductAPI/DbContexts/Models/Dto/Product.cs
+++ Mango.Services.ProductAPI/DbContexts/Models/Dto/Product.cs
@@ -7,7 +7,7 @@ namespace Mango.Services.ProductAPI.DbContexts.Models.Dto
         [Key]
         public int ProductId { get; set; }
         [Required]
-        public string ProductName { get; set; }
+        public string Name { get; set; }
         [Range(1, 1000)]
         public double Price { get; set; }
         public string Description { get; set; }
