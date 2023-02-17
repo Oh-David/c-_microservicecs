cd6634adca203bf359414e71c7cb1a03fcc06bab
David Oh <DavidOh@protonmail.com>
2023-02-16 20:58:25 -0800

    Add BaseService and ProductDtos for Mango.Web

---
+++ Mango.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<T> GetAllProductsAsync<T>();
+        Task<T> GetProductsByIdAsync<T>(string productId);
+        Task<T> CreateProductAsync<T>(ProductDto productDto);
+        Task<T> UpdateProductAsync<T>(ProductDto productDto);
+        Task<T> DeleteProductAsync<T>(string productId);
+    }
+}
